678e376ef7c9e41893a0cd60e4c40b1650b277be
2G2-99 <79936466+2G2-99@users.noreply.github.com>
2025-03-19 12:21:02 +0000

    DOM elements referenced

--- app/script.js
+++ app/script.js
@@ -1,12 +1,20 @@
+// # DOM
+const DISPLAY = document.querySelector('#display');
+const NUMBERS = document.querySelectorAll('.number');
+const OPERATORS = document.querySelectorAll('.operator');
+const ACTIONS = document.querySelectorAll('.action');
+
 // # OPERATIONS
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+////////
 let numberA = 3;
 let operator = '+';
 let numberB = 5;
+////////
 
 function handleOperation(a, operator, b) {
   switch (operator) {
@@ -26,3 +34,9 @@ function handleOperation(a, operator, b) {
 }
 
 console.log(handleOperation(numberA, operator, numberB));
+
+// DISPLAY
+const para = document.createElement('p');
+para.textContent = `${numberA} ${operator} ${numberB}`;
+
+DISPLAY.appendChild(para);
